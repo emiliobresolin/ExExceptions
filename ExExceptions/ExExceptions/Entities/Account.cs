524e98e1f8dad7d758773d8f2f4ef4d051165468
emiliobresolin <emiliotdm@hotmail.com>
2022-04-06 21:16:47 -0300

    exercise about exceptions

---
+++ ExExceptions/ExExceptions/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿using ExExceptions.Entities.Exceptions;
+
+namespace ExExceptions.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+        public Account() { }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit. BALANCE: " 
+                    + Balance + ". WITHDRAW LIMIT: " + WithdrawLimit + ".");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance. BALANCE: " + Balance + ". WITHDRAW LIMIT: " + WithdrawLimit + ".");
+            }
+            Balance -= amount;
+        }
+        public override string ToString()
+        {
+            return "New balance: " + Balance;
+        }
+    }
+}
---
+++ ExExceptions/ExExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using ExExceptions.Entities;
+using ExExceptions.Entities.Exceptions;
+
+namespace ExExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HOW MANY ACCOUNTS YOU WISH TO DO? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter account number #{i} data");
+                Console.Write("NUMBER: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("HOLDER: ");
+                string holder = Console.ReadLine();
+                Console.Write("INICIAL BALANCE: ");
+                double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("WITHDRAWLIMIT: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, holder, deposit, withdrawLimit);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                try
+                {
+                    account.Withdraw(amount);
+                    Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+                }
+                catch (DomainException e)
+                {
+                    Console.WriteLine("Withdraw error: " + e.Message);
+                }
+            }
+        }
+    }
+}
